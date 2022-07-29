902f1b6ede03641109ce4476c254fe7db08ee6b3
VabrantGames <vabrantgames@gmail.com>
2022-07-28 20:30:23 -0700

    init

---
+++ console/src/test/java/com/vabrant/playground/test/FolderStructureTest.java
@@ -0,0 +1,14 @@
+package com.vabrant.playground.test;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+public class FolderStructureTest {
+
+    @Test
+    public void createStructureTest() {
+        File file = new File("");
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
